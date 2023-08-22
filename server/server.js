f9215a548b3f6b133e8f52ab61bac58151efbe55
Ambica Khandelwal <khaashi123@gmail.com>
2023-08-22 19:46:13 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const cors = require('cors');
+
+app.use(cors());
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve the data.json file
+app.get('/api/userdata', (req, res) => {
+  res.sendFile(path.join(__dirname, 'data.json'));
+});
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
